2926509e94ae53f2999150d24569906a92ef39c8
Navanivous <Khaled2003yehia@gmail.com>
2023-05-05 15:06:23 +0300

    Store router + UserRouter pt1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine','ejs')
 app.set('views',__dirname +'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'/Resources/CSS/public')))
 
 app.use('/',indexRouter)
 
