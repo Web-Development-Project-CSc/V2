680d89f6566787ec9ce4c3317f1772411031e8e0
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-05-06 19:35:34 +0300

    Profile Menu Clicks

--- public/JS/navbar.js
+++ public/JS/navbar.js
@@ -9,3 +9,12 @@ function navscroll() {
     nav.classList.remove("sticky");
   }
 }
+let icon = document.getElementsByClassName('profileIcon');
+icon[0].addEventListener('click', function(){
+    let menu = document.getElementsByClassName('profileMenu');
+    if(menu[0].style.display == 'none'){
+        menu[0].style.display = 'block';
+    }else{
+        menu[0].style.display = 'none';
+    }
+});
