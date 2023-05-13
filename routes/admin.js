7937b07ea72bdc8da18d43509ef3418109ad53eb
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-05-13 19:44:04 +0300

    upload pic

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'./public/IMAGES/');
+    }
+    ,filename: function(req,file,cb){
+        cb(null,Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({storage: storage});
 
 router.get('/', (req,res)=>{
     res.render('dashboard',{layout: false})
@@ -15,5 +26,11 @@ router.get('/users', (req,res)=>{
 
 router.get('/store', (req,res)=>{
     res.render('store',{layout: false})
+})
+router.get('/upload', (req,res)=>{
+    res.render('upload',{layout: false})
+})
+router.post('/upload',upload.single('image'), (req,res)=>{
+    res.send('uploaded')
 })
 module.exports = router
