dc32e2a3599155a58894bc6e0b83b7b8423233fe
Navanivous <Khaled2003yehia@gmail.com>
2023-05-15 20:38:44 +0300

    modelssssssss

--- models/orders.js
+++ models/orders.js
@@ -21,13 +21,13 @@ const orderSchema = new mongoose.Schema({
             progress: function(stat){
                 this.status.name = stat;
                 if(stat === null){
-                    this.status.name = "Ordered";
+                    this.status.name = "Placed";
                     this.status.imgNum = 0; 
                 }
-                else if(stat === "In Progress"){
+                else if(stat === "Shipped"){
                     this.status.imgNum = 1
                 }
-                else if(stat === "Shipped"){
+                else if(stat === "Delivered"){
                 this.status.imgNum = 2;
                 }
                 else this.delivered = true;
