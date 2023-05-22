72cf59bb5900a19ededf10c93dc07560e5f4fc77
AbdulRahman Sherif <abdelrahmansherif630@gmail.com>
2023-05-22 19:33:00 +0300

    admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,47 +1,56 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const mongoose = require('mongoose');
+
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'./public/IMAGES/');
-    }
-    ,filename: function(req,file,cb){
-        cb(null,Date.now() + path.extname(file.originalname));
-    }
+  destination: function (req, file, cb) {
+    cb(null, './public/IMAGES/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
-const upload = multer({storage: storage});
-mongoose.connect('mongodb+srv://flavouredmiu:webproject123@cluster0.t6ylmgo.mongodb.net/?retryWrites=true&w=majority')
 
+const upload = multer({ storage: storage });
 
+mongoose.connect('mongodb+srv://flavouredmiu:webproject123@cluster0.t6ylmgo.mongodb.net/?retryWrites=true&w=majority');
 
-router.get('/', (req,res)=>{
-    if(req.session.user !== undefined && req.session.user.role==='admin')
-    res.render('dashboard',{layout: false}, { user: (req.session.user === undefined ? "" : req.session.user) })
-    else res.redirect('admin/login')
-})
+router.get('/', (req, res) => {
+  if (req.session.user !== undefined && req.session.user.role === 'admin') {
+    res.render('dashboard', { layout: false, user: req.session.user });
+  } else {
+    res.redirect('admin/login');
+  }
+});
 
-router.get('/products' , (req,res)=>{
-    if(req.session.user !== undefined && req.session.user.role==='admin')
-    res.render('products',{layout: false}, { user: (req.session.user === undefined ? "" : req.session.user) })
-    else res.redirect('login')
-})
+router.get('/products', (req, res) => {
+  if (req.session.user !== undefined && req.session.user.role === 'admin') {
+    res.render('products', { layout: false, user: req.session.user });
+  } else {
+    res.redirect('login');
+  }
+});
 
-router.get('/users', (req,res)=>{
-    if(req.session.user !== undefined && req.session.user.role==='admin')
-    res.render('users',{layout: false}, { user: (req.session.user === undefined ? "" : req.session.user) })
-    else res.redirect('login')
-})
+router.get('/users', (req, res) => {
+  if (req.session.user !== undefined && req.session.user.role === 'admin') {
+    res.render('users', { layout: false, user: req.session.user });
+  } else {
+    res.redirect('login');
+  }
+});
 
+router.get('/login', (req, res) => {
+  res.render('login', { layout: false, user: req.session.user || '' });
+});
 
-router.get('/login', (req,res)=>{
-    res.render('login',  { layout: false}, { user: (req.session.user === undefined ? "" : req.session.user) })
+router.post('/products', upload.single('image'), (req, res) => {
+  if (req.session.user !== undefined && req.session.user.role === 'admin') {
+    res.render('products', { layout: false, user: req.session.user });
+  } else {
+    res.redirect('login');
+  }
+});
 
-})
-router.post('/products',upload.single('image'), (req,res)=>{
-    if(req.session.user !== undefined && req.session.user.role==='admin')
-    res.render('products',{layout: false}, { user: (req.session.user === undefined ? "" : req.session.user) })
-    else res.redirect('login')
-})
-module.exports = router
+module.exports = router;
