49ba571e9e38e4666174ea6263570caabef1cecb
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-05-22 22:17:04 +0300

    FindUser controllers

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const mongoose = require('mongoose');
 const Accounts = require('../models/accounts');
+const findUser = require('../controllers/findUser');
 router.use(express.urlencoded({extended:true}))
 mongoose.connect('mongodb+srv://flavouredmiu:webproject123@cluster0.t6ylmgo.mongodb.net/Flavoured').then(result =>{console.log("connected")}).catch(err => {console.log(err)})
 router.get('/', (req,res)=>{
@@ -17,29 +18,6 @@ router.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
   });
-  router.post('/logging', (req,res)=>{
-    let user = {
-        email: req.body.email,
-        password: req.body.psw
-    }
-    console.log(user);
-Accounts.findOne( user ).then(result => {
-if(result){
-    req.session.user = result;
-    console.log(req.session.user);
-    console.log("logged in");
-    if(req.session.user !== undefined && req.session.user.role==='admin') 
-    res.redirect('/admin');
-    else if(req.session.user !== undefined && req.session.user.role==='customer')
-    res.redirect('/user/myprofile');
-}
-else {
-    console.log(result)
-    console.log("not logged in");
-    res.redirect('/');
-}}).catch(err => {
-    console.log(err);
-    res.render('notfound',{layout:false});
-})})
+  router.post('/logging', findUser.findUser)
    
 module.exports = router
