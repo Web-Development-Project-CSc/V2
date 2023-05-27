a014be0987137fccdc014f067e8df5c63bce4035
Navanivous <Khaled2003yehia@gmail.com>
2023-05-27 20:19:02 +0300

    dynamic store

--- models/products.js
+++ models/products.js
@@ -1,7 +1,8 @@
+const mongoose= require('mongoose');
 const productSchema= new mongoose.Schema({
     name:String,
     price:Number,
     numPurchases:Number,
     imgUrl:String,
 }, {timestamps:true})
-module.exports= mongoose.model('Product', productSchema)
+module.exports= mongoose.model('products', productSchema)
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const mongoose = require('mongoose');
 const Accounts = require('../models/accounts');
+const Products = require('../models/products');
 const findUser = require('../controllers/findUser');
 router.use(express.urlencoded({extended:true}))
 mongoose.connect('mongodb+srv://flavouredmiu:webproject123@cluster0.t6ylmgo.mongodb.net/Flavoured').then(result =>{console.log("connected")}).catch(err => {console.log(err)})
@@ -12,7 +13,12 @@ router.get('/support', (req,res)=>{
     res.render('help' ,{ user: (req.session.user === undefined ? "" : req.session.user) })
 })
 router.get('/store', (req,res)=>{
-    res.render('store', { user: (req.session.user === undefined ? "" : req.session.user) })
+    let prods
+   Products.find().then(result =>{
+    prods=result
+    res.render('store', { user: (req.session.user === undefined ? "" : req.session.user) , prods: prods})
+    }).catch(err => {console.log(err)}).then(
+   )
 });
 router.get('/logout', (req, res) => {
     req.session.destroy();
