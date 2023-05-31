8b6c852e3f361ed7f3fccb7452c5af613eda7b3d
AbdulRahman Sherif <abdelrahmansherif630@gmail.com>
2023-05-31 19:08:22 +0300

    usersearch admin

--- public/JS/usersearch.js
+++ public/JS/usersearch.js
@@ -1,53 +1,59 @@
+let titles = document.querySelectorAll('.userData td');
 
 function sendData(e) {
-    const searchResults = document.getElementById('searchResults');
-    let match = e.value.match(/^[a-zA-Z ]*/);
-    let match2 = e.value.match(/^\s*$/);
-    
-    if (match2 && match2[0] === e.value) {
-      searchResults.innerHTML = '';
-      return;
-    }
-    else if (match && match[0] === e.value) {
-      fetch('/admin/getUserResults', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ payload: e.value })
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          let payload = data.payload;
-          searchResults.innerHTML = '';
-          
-          if (payload.length < 1) {
-            searchResults.innerHTML = '<p>Sorry, Nothing Found.</p>';
-            return;
-          }
-          
-          payload.forEach((item, index) => {
-            const p = document.createElement('p');
-            p.innerHTML = item.name;
-            p.addEventListener('click', () => {
-              document.querySelector("#itemclicked").value = item.name;
-              document.querySelector("#searchInput").value = item.name;
-              searchResults.innerHTML = '';
-            });
-            searchResults.appendChild(p);
-          });
-          use();
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    }
-    
+  const searchResults = document.getElementById('searchResults');
+  let match = e.value.match(/^[a-zA-Z ]*/);
+  let match2 = e.value.match(/^\s*$/);
+
+  if (match2 && match2[0] === e.value) {
     searchResults.innerHTML = '';
+    return;
+  } else if (match && match[0] === e.value) {
+    fetch('/admin/getUserResults', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ payload: e.value })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        let payload = data.payload;
+        searchResults.innerHTML = '';
+
+        if (payload.length < 1) {
+          searchResults.innerHTML = '<p>Sorry, Nothing Found.</p>';
+          return;
+        }
+
+        payload.forEach((item, index) => {
+          const p = document.createElement('p');
+          p.innerHTML = item.name;
+          p.addEventListener('click', () => {
+            document.querySelector("#itemclicked").value = item.name;
+            document.querySelector("#searchInput").value = item.name;
+            searchResults.innerHTML = '';
+            
+            for(let i =0; i < titles.length; i++) {
+              if(titles[i].innerHTML == p.innerHTML)
+              window.scrollTo(titles[i].offsetLeft, titles[i].offsetTop);
+            }
+          });
+          searchResults.appendChild(p);
+        });
+        use();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
-  function use(){
-  let res = document.querySelector("#searchResults p")
-  for(let i=0;i<res.length;i++){
-    res[i].addEventListener('click',()=>{
-     
+
+  searchResults.innerHTML = '';
+}
+
+function use() {
+  let res = document.querySelectorAll("#searchResults p")
+  for (let i = 0; i < res.length; i++) {
+    res[i].addEventListener('click', () => {
+
     })
   }
-  }
+}
