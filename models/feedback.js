55674206cf4dac58c9f7229692e896a0ed99374b
Raghda Elkhatib <125472396+Raghdakk@users.noreply.github.com>
2023-05-31 19:12:29 +0200

    .

--- models/feedback.js
+++ models/feedback.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 const { boolean } = require('webidl-conversions')
 const feedbackSchema = new mongoose.Schema(
     {
-        
             product:
             {
                 type: mongoose.Schema.Types.ObjectId,
                 ref:"products"
+            },
+            customer:
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:"accounts"
             },
         }  
 )
