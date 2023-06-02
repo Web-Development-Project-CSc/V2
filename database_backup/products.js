9283c183e1739add9ed900f0b3d111fb89ba6242
Raghda Elkhatib <125472396+Raghdakk@users.noreply.github.com>
2023-06-02 13:18:47 +0200

    .

--- database_backup/products.js
+++ database_backup/products.js
@@ -2,7 +2,23 @@ const Products = require('../models/products');
 
 const products = [
     {
-
+        "name": "Cheetos",
+        "price": 16.75,
+        "numPurchases": 4,
+        "imgUrl": "/IMAGES/Flavours/Cheetos.jpg",
+        "updatedAt": {
+          "$date": "2023-05-31T09:20:58.706Z"
+        }
     },
+    {
+        "name": "Borio",
+        "price": 12,
+        "numPurchases": 0,
+        "imgUrl": "/IMAGES/Flavours/borio.png"
+      },
+    {
+        
+    }
     
+
 ]
