0ea0083117a884664ef6307883a59a2e2ad3913f
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-06-06 13:35:57 +0300

    email

--- controllers/sendgrid.js
+++ controllers/sendgrid.js
@@ -256,7 +256,7 @@ Please contact us at flavouredmiu@gmail.com if you require any further assistanc
     html: page,
 }
 sgMail.send(message).then( response => console.log('Mail sent successfully')).catch(error => console.log(error.message));
-res.redirect('/user/confirmation');
+res.redirect('/');
 }
 else res.redirect("/user/login?message='Must be logged in to view this page'");
 }
