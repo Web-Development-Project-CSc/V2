9e2128ee2e4e1385c0cdb2d0bbcb7e518b6815f3
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-06-07 21:53:53 +0300

    query

--- controllers/ctrlFAQ&Requests.js
+++ controllers/ctrlFAQ&Requests.js
@@ -3,6 +3,7 @@ const Accounts = require('../models/accounts');
 const Requests = require('../models/requests');
 const addFAQ = async (req,res)=>{
     Accounts.findOne({email: req.body.email}).then(async result=>{
+        if(result){
     const faq = new FAQs({
         customer: result._id,
         question: req.body.question
@@ -15,7 +16,9 @@ const addFAQ = async (req,res)=>{
         console.log(err);
         res.redirect("/support?message='Could not register query'");
     }
-    })   
+    }
+    else res.redirect("/support?message='Could not register query, Must be logged in first'");
+})   
 }
 const addRequest = async (req,res)=>{
     const reqs = await Requests.findOneAndUpdate({flavourName: req.body.flavourName}, {$inc: {number: 1}})
